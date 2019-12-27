1ec4dad32181c662e7ac73f15741ef1ce6356cc9
Sen <simon48105@gmail.com>
2019-12-26 23:05:01 -0500

    Added a stats command that checks the current stats of the currency over the past 24 hours Stats include:   highest price   lowest price   volume transferred   current price

--- prices.py
+++ prices.py
@@ -7,6 +7,7 @@ import os
 import random
 import requests
 import time
+import urllib.request
 
 #response = requests.get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
 
@@ -18,15 +19,41 @@ class prices:
     @commands.command()
     @commands.cooldown(rate=1, per=2.0)
     async def price(self, *argv): #argv[0] is the crypto name(eg; BTC, ETH, LINK, etc.) and argv[1] is the currency code(eg: USD, EUR, etc.)
+        cryptoID = argv[0] + "-" + argv[1]
         colors = [0xff0000, 0xff8100, 0xfdff00, 0x15ff00, 0x15ff00, 0x0045ff, 0x9600ff, 0xff00b4]        
-        response = requests.get("https://api.coinbase.com/v2/prices/"+ argv[0] + "-" + argv[1] +"/spot")
+        response = requests.get("https://api.pro.coinbase.com/products/"+ cryptoID +"/stats")
         data = response.json()
-        currency = data["data"] ["base"]
-        ogCurrency = data["data"] ["currency"]
-        price = data["data"] ["amount"]
-        embed = discord.Embed(name=currency, description='The price of ' + currency + " in " + ogCurrency, color=random.choice(colors))
+        price = data["last"]
+        embed = discord.Embed(name=argv[0], description='The price of ' + argv[0] + " in " + argv[1], color=random.choice(colors))
         embed.add_field(name='Price', value=price, inline=False)
         await self.bot.say(embed=embed)
+
+
+    #get 24 hours stats for whatever crypto the user indicates
+    @commands.command()
+    @commands.cooldown(rate=1, per=2.0)
+    async def stats(self, *argv):
+        cryptoID = argv[0] + "-" + argv[1]
+        colors = [0xff0000, 0xff8100, 0xfdff00, 0x15ff00, 0x15ff00, 0x0045ff, 0x9600ff, 0xff00b4]        
+        response = requests.get("https://api.pro.coinbase.com/products/"+ cryptoID +"/stats")
+        data = response.json()
+        high = data["high"]
+        low = data["low"]
+        volume = data["volume"]
+        current = data['last']
+        embed = discord.Embed(name=argv[0], description="24 Hours stats for " + argv[0], color=random.choice(colors))
+        embed.add_field(name='Current', value=current, inline=True)
+        embed.add_field(name='High', value=high, inline=True)
+        embed.add_field(name='Low', value=low, inline=True)
+        embed.add_field(name='Amount Transferred', value=volume, inline=True)
+        await self.bot.say(embed=embed)
+
+
+
+#nomics shit
+#api key: a51b49815a3f42732b6fbfd7dbcb6139
+
+
 
 def setup(bot):
     bot.add_cog(prices(bot))
